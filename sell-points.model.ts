3402d2174ecb10dfa19b23174af4d835bd9c675f
markos0886 <markos0886@gmail.com>
2020-05-23 17:46:43 -0400

    Eliminando ficheros de codigo de la raiz del proyecto accidentalmente subidos

--- sell-points.model.ts
+++
@@ -1,7 +0,0 @@
-export class PointSell{
-  constructor(
-    public seller: Number,
-    public buyer: String,
-    public amount: Number,
-  ) { }    
-}
--- tipo-opcion.model.ts
+++
@@ -1,9 +0,0 @@
-export class TipoOpcionModel {
-  constructor(public TipoOpcionId: number,
-              public Nombre: string,
-              public NombreCodigo: string,
-              public TextoLabel: string,
-              public Precio: number,
-              public CantidadFrecuencia: number,
-              public MinimoComprar: number) {}
-}
